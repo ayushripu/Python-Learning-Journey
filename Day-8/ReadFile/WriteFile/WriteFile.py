edf3af27ef2899a7f0fde912b71b5f2ce7865e0c
Ayush Kumar <ayushbbu37@gmail.com>
2026-02-25 01:18:10 +0530

    first commit

---
+++ Day-8/ReadFile/WriteFile/WriteFile.py
@@ -0,0 +1,7 @@
+f = open("C:\\Users\\ayush\\OneDrive\\Desktop\\Python\\Day-8\\ReadFile\\itself.txt", "r")
+
+
+d = f.write("This is the file itself. It is used to demonstrate how to write to a file in Python.")
+
+print(d)
+f.close()
